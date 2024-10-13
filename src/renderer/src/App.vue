70192daba896e1a4c499a7de871cd46e34c17909
quyang <1633433173@qq.com>
2024-10-13 22:14:46 +0800

    初始化项目

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Versions from './components/Versions.vue'
+
+const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+</script>
+
+<template>
+  <div>我的第一个桌面应用！！！</div>
+</template>
