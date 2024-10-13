4de45962005e578464de47c3455a6c458a62ed1d
quyang <1633433173@qq.com>
2024-10-13 23:55:49 +0800

    登录注册页面

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,9 +1,18 @@
 <script setup>
-import Versions from './components/Versions.vue'
+import { ref, reactive, getCurrentInstance, nextTick } from 'vue'
+const { proxy } = getCurrentInstance();
 
-const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+import { ElConfigProvider } from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
+const locale = zhCn;
+const config = reactive({
+  max: 1
+})
 </script>
 
 <template>
-  <div>我的第一个桌面应用！！！</div>
+  <el-config-provider :locale="locale" :message="config">
+    <router-view></router-view>
+  </el-config-provider>
 </template>
---
+++ src/renderer/src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <el-button type="primary" @click="">按钮</el-button>
+    </div>
+</template>
+
+<script setup>
+import { ref, reactive, getCurrentInstance, nextTick } from "vue"
+const { proxy } = getCurrentInstance();
+
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
